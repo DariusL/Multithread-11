29c9c99e4f5ab012cced374f6c1bba6bf841ab91
darlap1 <darlap1@ifb305-008.sc.ad.ktu.lt>
2013-09-09 16:29:51 +0300

    Fuck it

--- App/src/app/App.java
+++ App/src/app/App.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     private class Struct{
         public String pav;
@@ -9,18 +14,84 @@ public class App {
         public static final String PAV = "Pavadinimas";
         public static final String KIEKIS = "Kiekis";
         public static final String KAINA = "Kaina";
+        public static final String DELIMS = "[ ]+";
+        
+        Struct(String input){
+            String[] gabalai = input.split(DELIMS);
+            pav = gabalai[0];
+            kiekis = Integer.valueOf(gabalai[1]);
+            kaina = Double.valueOf(gabalai[2]);
+        }
 
         @Override
         public String toString() {
-            return String.format("20%s, 5%d, 10%f", pav, kiekis, kaina);
-        }
-        
-        public String antraste(){
-            return String.format("20%s, 5%s, 10%s", PAV, KIEKIS, KAINA);
+            return String.format("%20s, %6d, %10f", pav, kiekis, kaina);
         }
     }
     
+    public static String[] readLines(String filename){
+        try{
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            List<String> lines = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+            bufferedReader.close();
+            return lines.toArray(new String[lines.size()]);
+        }catch(Exception e){
+            return new String[0];
+        }
+    }
+    
+    public Struct[] skaitymas(String failas){
+        String[] duomenai = readLines("duomenys.txt");
+        Struct[] duomenys = new Struct[duomenai.length];
+        for(int i = 0; i < duomenys.length; i++)
+            duomenys[i] = new Struct(duomenai[i]);
+        return duomenys;
+    }
+    
+    public void spausdinti(Struct[] duomenai, String prefix){
+        System.out.print(antraste());
+        for(int i = 0; i < duomenai.length; i++)
+            System.out.println(prefix + i + " " + duomenai[i].toString());
+    }
+    
+    public void otherMain(){
+        int procesuSkaicius = 3;
+        final Struct[] duomenys = skaitymas("duomenys.txt");
+        String[] procesaiTmp = new String[procesuSkaicius];
+        for(int i = 0; i < procesuSkaicius; i++){
+            procesaiTmp[i] = "Procesas" + i + " ";
+        }
+        final String[] procesuPav = procesaiTmp;
+        List<Thread> procesai = new ArrayList(procesuSkaicius);
+        antraste();
+        spausdinti(duomenys, "");
+        System.out.print("\n\n\n");
+        for(int i = 0; i < procesuSkaicius; i++){
+            final int nr = i;
+            procesai.add(new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    spausdinti(duomenys, procesuPav[nr]);
+                }
+            }));
+        }
+        for(Thread t : procesai){
+            t.start();
+        }
+    }
+    
+    public static String antraste(){
+            return String.format("%22s, %5s, %10s\n", Struct.PAV, Struct.KIEKIS, Struct.KAINA);
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        App app = new App();
+        app.otherMain();
     }
 }
